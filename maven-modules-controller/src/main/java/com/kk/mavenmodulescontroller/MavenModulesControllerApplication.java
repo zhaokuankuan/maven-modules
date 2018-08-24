6908b8cb5bf25961a6ec95d2d3aba4295ee88a8b
赵宽宽 <zhaokk@yonyou.com>
2018-08-24 17:29:10 +0800

    提交代码

--- maven-modules-controller/src/main/java/com/kk/mavenmodulescontroller/MavenModulesControllerApplication.java
+++ maven-modules-controller/src/main/java/com/kk/mavenmodulescontroller/MavenModulesControllerApplication.java
@@ -3,9 +3,11 @@ package com.kk.mavenmodulescontroller;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("com.kk")
+@ComponentScan("com.kk.*")
 public class MavenModulesControllerApplication {
 
 	public static void main(String[] args) {
