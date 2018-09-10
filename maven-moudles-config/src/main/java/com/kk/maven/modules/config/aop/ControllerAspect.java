067339d4296252a74ee7ecc58f404e7a1cc47c7e
赵宽宽 <zhaokk@yonyou.com>
2018-09-10 14:22:38 +0800

    增加文档的说明

--- maven-moudles-config/src/main/java/com/kk/maven/modules/config/aop/ControllerAspect.java
+++ maven-moudles-config/src/main/java/com/kk/maven/modules/config/aop/ControllerAspect.java
@@ -10,13 +10,31 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.BindingResult;
 
-/** aop的文件
+/** AOP的文件
  * @author :Mr.kk
  * @date: 2018/9/10 13:27
  */
 @Component
 @Aspect
 public class ControllerAspect {
+
+
+    /***
+    execution()是最常用的切点函数，其语法如下所示：
+
+    整个表达式可以分为五个部分：
+
+    1、execution(): 表达式主体。
+
+    2、第一个*号：表示返回类型， *号表示所有的类型。
+
+    3、包名：表示需要拦截的包名，后面的两个句点表示当前包和当前包的所有子包，com.sample.service.impl包、子孙包下所有类的方法。
+
+    4、第二个*号：表示类名，*号表示所有的类。
+
+    5、*(..):最后这个星号表示方法名，*号表示所有的方法，后面括弧里面表示方法的参数，两个句点表示任何参数
+     **/
+
 
     private final Logger LOG = LoggerFactory.getLogger(this.getClass());
 
